3395015359285956b58917fa609b951473a18565
Caroline Chin <cmchin@mit.edu>
2015-03-27 00:22:58 -0400

    created sample documents
    to use, go to /Sample in terminal and type "node nodeFile.js"
    in your browser, type localhost:1337/test.html
    
    created database of bus routes of public data from Chicago Transportation
    	- see /Sample/Data/sql_practice.py to see how I made the database
    created an html and js file that returns streets on a route based on user input
    	- see /Sample/test.html
    	- and /Sample/nodeFile.html
    use it to for examples of  how to use jquery, node, sql

---
+++ Sample/nodeFile.js
@@ -0,0 +1,38 @@
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+//Load modules
+var sqlite3         =       require('sqlite3').verbose();
+var db              =       new sqlite3.Database('./Data/chicago.sqlite');
+
+app.use(express.static(__dirname)); 
+
+function findRouteNumber(routeNumber, response){
+	// logs the routeNumber in the console
+	// to see the console, look at your terminal window where you ran nodeFile.js
+	console.log(routeNumber);
+	// selects the relevant variable "on_street" from the table in the database, called "Bus", 
+	// where the route is equal to the routeNumber
+	db.all('select on_street from Bus where routes = "'+routeNumber+'"', function(err, streets){
+		// send the first street name back to the browser
+		response.send(streets[0]);
+	})
+}
+
+
+// handle requests from client
+app.get('/',function(request,response){
+	response.header('Access-Control-Allow-Origin', "*");
+	
+	if (request.query.type == "routeStreetRequest"){
+		var output = findRouteNumber(request.query.routeNumber, response);
+	}
+
+});
+
+app.listen(1337);
+console.log('Listening on port 1337');
+
+
