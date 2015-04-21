6b690de6d470698edab898a4b7d9932e52a7ee76
Caroline Chin <cmchin@mit.edu>
2015-04-21 11:00:06 -0400

    added more realistic problems to the database modified dragging to be able to drag into 'empty' lists

---
+++ Sample/WTE/customize.js
@@ -0,0 +1,16 @@
+$(function() {
+   var oldList, newList, item;
+   $('.customizeSortable').sortable({
+       start: function(event, ui) {
+           item = ui.item;
+           newList = oldList = ui.item.parent().parent();
+       },
+       stop: function(event, ui) {          
+           alert("Moved " + item.text() + " from " + oldList.attr('id') + " to " + newList.attr('id'));
+       },
+       change: function(event, ui) {  
+           if(ui.sender) newList = ui.placeholder.parent().parent();
+       },
+       connectWith: ".customizeSortable"
+   }).disableSelection();
+});
